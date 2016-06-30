38a462db617cedce99b01a14b3e4df478ae36fb0
patricktisdel@outlook.com <timeof26>
2016-06-30 18:32:41 -0400

    Uploading node module prerequisites

--- app/app.component.js
+++ app/app.component.js
@@ -1,10 +1,45 @@
 (function(app) {
-  app.AppComponent =
+    
+    
+  
+    
+    
+    var HorseList =
+    ng.core.Component({
+      selector: 'horse-list',
+      template: `
+
+<ul>
+    <li *ngFor="let horse of horses">{{horse.name}}</li>
+</ul>
+`,
+        directives: [ng.common.NgFor]
+
+    })
+    .Class({
+      constructor: function() {
+          this.horses = [
+              {name:"Bobby", gender:"male"},
+              {name:"Caleb", gender:"male"},
+              {name:"Jen", gender:"female"}
+          ];
+      }
+    });
+    
+    
+    
+    
+    app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>My First Angular 2 App</h1>'
+      template: '<h1>My First Angular 2 App</h1><horse-list></horse-list>',
+      directives: [HorseList]
     })
     .Class({
       constructor: function() {}
     });
+    
+    
+    
+    
 })(window.app || (window.app = {}));
